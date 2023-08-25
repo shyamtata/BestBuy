35f3e608cef6c1655584012c201d74b4e65ff823
shyam <shyamtata1533@gmail.com>
2023-08-25 14:55:34 -0500

    first commit

---
+++ bestbuy/bestbuy/src/main/java/model/Order.java
@@ -0,0 +1,77 @@
+package model;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date orderDate;
+
+    private double totalAmount;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+    private List<OrderItem> orderItems;
+
+    // Constructors
+    public Order() {
+    }
+
+    public Order(User user, Date orderDate, double totalAmount, List<OrderItem> orderItems) {
+        this.user = user;
+        this.orderDate = orderDate;
+        this.totalAmount = totalAmount;
+        this.orderItems = orderItems;
+    }
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Date getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(Date orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public double getTotalAmount() {
+        return totalAmount;
+    }
+
+    public void setTotalAmount(double totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+
+    public List<OrderItem> getOrderItems() {
+        return orderItems;
+    }
+
+    public void setOrderItems(List<OrderItem> orderItems) {
+        this.orderItems = orderItems;
+    }
+}
+
---
+++ bestbuy/bestbuy/src/test/java/com/shopping/bestbuy/BestbuyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopping.bestbuy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestbuyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
